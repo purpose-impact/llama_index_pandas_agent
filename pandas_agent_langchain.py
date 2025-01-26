581b2eb127533eb1d4e041bf8988750885489ee0
Kumar Raju <107989985+purpose-impact@users.noreply.github.com>
2025-01-26 21:19:11 +0000

    Update pandas_agent_langchain.py

--- pandas_agent_langchain.py
+++ pandas_agent_langchain.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from langchain_experimental.agents import create_pandas_dataframe_agent
 from dotenv import load_dotenv
 import pandas as pd
@@ -18,10 +19,44 @@ agent = create_pandas_dataframe_agent(
     llm, df, agent_type="openai-tools", verbose=True, allow_dangerous_code=True
 )
 
-response = agent.invoke(
-    {
-        "input": "list all the details about the company name !BIG IMPACT GRAPHICS LIMITED?"
-    }
-)
+#response = agent.invoke(
+#    {
+#        "input": "What is the FULL ADDRESS  of company !BIG IMPACT GRAPHICS LIMITED?"
+#    }
+#)
 
-print(response)
+def get_response(prompt_input):
+        chat_completion = agent.invoke (
+             {
+                "input": prompt_input
+             }
+        )
+        response = chat_completion.get("output")
+        print(response)
+        return response
+
+
+
+# Initialize chat history
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+# Streamlit app layout
+st.title("Companies House Chatbot")
+user_input = st.text_input("You:", key="input")
+
+if user_input:
+    # Append user input to chat history
+    st.session_state.chat_history = []
+    st.session_state.chat_history.append({"role": "user", "content": user_input})
+
+    # Generate response from Groq model
+    response = get_response(user_input)
+    st.session_state.chat_history.append({"role": "assistant", "content": response})
+
+# Display chat history
+for message in st.session_state.chat_history:
+    st.write(f"{message['role'].capitalize()}: {message['content']}")
+
+
+#print(response)
